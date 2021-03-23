53c79b22f41e9cbc89923404570d4d69b20a45f6
yerminal <60397218+yerminal@users.noreply.github.com>
2021-03-23 20:01:55 +0300

    Update main.py

--- METU_ADD_DROP_BOT/main.py
+++ METU_ADD_DROP_BOT/main.py
@@ -4,15 +4,28 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 import time
 import winsound
-import getpass
-import pickle, os
-from fake_useragent import UserAgent
 
 # PARAMETERS
 metu_username = ''  # Your METU username - eXXXXXX - e204849
 metu_password = ''  # Your METU password
 
-priorList = [63, 61, 13, 14, 15, 90]
+courseCode = '6390102'
+sectionCode = '95'  # CURRENT SECTION CODE
+orderOfCourse = 7
+categoryCode = '1'
+
+"""
+COURSE CATEGORIES:
+
+MUST = 1
+RESTRICTED ELECTIVE = 4
+FREE ELECTIVE = 5
+TECHNICAL ELECTIVE = 7
+NONTECHNICAL ELECTIVE = 8
+NOT INCLUDED = 9
+"""
+
+priorList = [63, 61, 13, 14, 15, 90]  # Section list which you want to enter
 
 duration = 1000  # milliseconds
 freq = 440  # Hz
@@ -27,17 +40,11 @@ def start_process(driver, wait):
 # SETUP AND LAUNCH
 options = webdriver.ChromeOptions()
 options.add_argument("start-maximized")
-#options.add_argument(f"user-data-dir=C:\\Users\\{getpass.getuser()}\\AppData\\Local\\Google\\Chrome\\User Data")
-ua = UserAgent()
-userAgent = ua.random
-#options.add_argument(f'user-agent={userAgent}')
-
 options.add_experimental_option("excludeSwitches", ["enable-automation"])
 options.add_experimental_option('useAutomationExtension', False)
 driver = webdriver.Chrome(options=options, executable_path=r'C:\WebDrivers\chromedriver.exe')
-driver.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")
 
-wait = WebDriverWait(driver, 10)
+wait = WebDriverWait(driver, timeout=10)
 start_process(driver, wait)
 
 """WebDriverWait(driver, 10).until(EC.frame_to_be_available_and_switch_to_it((By.CSS_SELECTOR,"iframe[name^='a-'][src^='https://www.google.com/recaptcha/api2/anchor?']")))
@@ -54,16 +61,17 @@ while not done:
         while not done:
             for i in priorList:
                 driver.find_element_by_xpath(
-                    '//*[@id="single_content"]/form/table[3]/tbody/tr[1]/td/table/tbody/tr[8]/td[1]/input').click()
+                    f'//input[@value="{courseCode}|{sectionCode}|{categoryCode}"]').click()
                 driver.find_element_by_xpath('//*[@id="textChangeCourseSection"]').clear()
                 driver.find_element_by_xpath('//*[@id="textChangeCourseSection"]').send_keys(str(i))
                 driver.find_element_by_xpath(
-                    '//*[@id="single_content"]/form/div[2]/div/center/fieldset/div/table/tbody/tr[2]/td[2]/input').click()
+                    '//input[@name="submitChangeSection"]').click()
                 if driver.find_element_by_xpath(
-                        '//*[@id="single_content"]/form/table[3]/tbody/tr[1]/td/table/tbody/tr[8]/td[5]').text == str(i):
+                        f'//*[@id="single_content"]/form/table[3]/tbody/tr[1]/td/table/tbody/tr[{orderOfCourse+1}]/td[5]').text == str(i):
                     done = True
                     break
                 time.sleep(0.2)
+
     except:
         print("Restarting...")
         start_process(driver, wait)
